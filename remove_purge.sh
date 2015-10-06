4a3cae2d7fc2b9f6fce9888de383cc6cc2139fd4
pengshp <pengshp3@outlook.com>
2015-10-06 17:29:47 +0800

    add autoremove

--- remove_purge.sh
+++ remove_purge.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #Function:卸载不必要的配置文件
 #说明：请以root 运行
-echo  `date`
+date
 echo "正在清除......"
 sudo dpkg -l|grep "^rc"|awk '{print $2}' |xargs aptitude -y purge
-
+sudo apt-get autoremove
+sudo apt-get clean
 echo "残留的配置文件已清除！"
